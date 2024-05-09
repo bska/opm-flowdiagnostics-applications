aeeb35ba681269657cb107b5c736ad5a49c19390
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2024-05-09 19:08:19 +0200

    Make Component Independent of OPM-Common
    In particular, switch to using a self-hosted build system which
    depends only on CMake and which uses Boost.Test as its unit
    testing framework.  Import a copy of the Parameter* library from
    OPM-Common and bring in LibECL, version 2.7.0, through the CMake
    module Fetch_Content.

--- tests/test_eclregionmapping.cpp
+++ tests/test_eclregionmapping.cpp
@@ -26,9 +26,7 @@
 
 #define BOOST_TEST_MODULE TEST_REGION_MAPPING
 
-#include <opm/common/utility/platform_dependent/disable_warnings.h>
 #include <boost/test/unit_test.hpp>
-#include <opm/common/utility/platform_dependent/reenable_warnings.h>
 
 #include <opm/utility/ECLRegionMapping.hpp>
 
